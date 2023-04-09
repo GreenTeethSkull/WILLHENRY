8861b3963bb63296e37ad6ae00a8ca57530278c0
GreenTeethSkull <angelitorios366@gmail.com>
2023-04-08 20:25:30 -0500

    resolution of excercise 9

--- 09.js
+++ 09.js
@@ -24,7 +24,15 @@ function filtrar(funcion) {
   // productos.filtrar(function(p) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
-
+  Array.prototype.filtrar = function (callback) {
+    let arr = [];
+    for (let elm of this) {
+      if (callback(elm)) {
+        arr.push(elm);
+      }
+    };
+    return arr;
+  }
 };
 
 // No modifiques nada debajo de esta linea //
